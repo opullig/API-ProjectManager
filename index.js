6ba0d8e64663f31a72b8080e2938a52cdf3cb329
Renan Pullig <contato@opullig.com.br>
2023-11-22 14:18:13 -0300

    Create a Middleware for test if project exists

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const server = express();
 const DATABASE = require('./database.json') 
 server.use(express.json())
 
+//MIDDLEWARES
+
+function thisProjectExists(req, res, next){
+  const {id} = req.params
+  const project = DATABASE.find(project => project.id_project == id)
+  if(!project) 
+    return res.json({Message: "Project does not exists"})
+  
+  next()
+}
+
+//ROUTES
 server.get('/projects', (req, res)=> res.json(DATABASE))
 
-server.get('/projects/:id', (req,res)=>{
+server.get('/projects/:id', thisProjectExists, (req,res)=>{
   const {id} = req.params
   return res.json(DATABASE[id])
 })
@@ -22,7 +34,7 @@ server.post('/projects', (req, res) => {
   return res.status(200).send();
 })
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', thisProjectExists, (req, res) => {
   const { id } = req.params;
   const { title, description} = req.body;
   DATABASE[id]= {
@@ -34,7 +46,7 @@ server.put('/projects/:id', (req, res) => {
   return res.status(200).send();
 })
 
-server.post('/projects/:id/tasks', (req, res)=>{
+server.post('/projects/:id/tasks', thisProjectExists, (req, res)=>{
   const {id} = req.params;
   const {tasks} = req.body;
   tasks.forEach(task => DATABASE[id].tasks.push(task));
@@ -42,7 +54,7 @@ server.post('/projects/:id/tasks', (req, res)=>{
   return res.status(200).send();
 });
 
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', thisProjectExists, (req, res) => {
   const { id } = req.params;  
   DATABASE.splice(id, 1);
 
